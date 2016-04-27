8ec02ed827f5b2e6f51953c6d35e3387335d0cec
samjbro <sjbroughton1@gmail.com>
2016-04-27 17:29:25 +0100

    fixed the problem (partly) - see comments in file

--- map.rb
+++ map.rb
@@ -4,15 +4,13 @@ require 'ostruct'
 
 json = File.read('input.json')
 obj = JSON.parse(json)
-
-
-obj["locations"].each do |loc| 
-
-	#loc["env"] = "forest"
+obj["locations"].each do |key, val| #using .each on a hash expects |key, val| rather than just |item| like on an array
+	val["env"] = "forest" #val is the key {"env"=>""}, so val["env"] targets the vale you want
 end
-#print obj["locations"]["00"]
+
+#This works, but adds it to the end of the existing data.
 File.open("input.json","a") do |f|
-  #f.write(obj.to_json)
+  f.write("\n#{obj.to_json}") #This now adds the data to a new line - just makes it easier to read, doesn't fix the problem
 end
 
 
@@ -20,3 +18,4 @@ def make_map()
 
 
 end
+
