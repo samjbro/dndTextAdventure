0442fbea58215d59fceb822f16bfe031dd30ea72
samjbro <sjbroughton1@gmail.com>
2016-04-27 17:32:07 +0100

    fixed the problem (partly) - see comments in file

--- map.rb
+++ map.rb
@@ -8,7 +8,7 @@ obj["locations"].each do |key, val| #using .each on a hash expects |key, val| ra
 	val["env"] = "forest" #val is the key {"env"=>""}, so val["env"] targets the vale you want
 end
 
-#This works, but adds it to the end of the existing data.
+#This works, but adds it to the end of the existing data - subsequent attempts to load the json file will therefore fail
 File.open("input.json","a") do |f|
   f.write("\n#{obj.to_json}") #This now adds the data to a new line - just makes it easier to read, doesn't fix the problem
 end
